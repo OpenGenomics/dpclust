d76ce6576495ff1642557107830294dd8267d87d
Kami Chiotti <chiotti@ohsu.edu>
2021-05-19 12:53:06 -0700

    Fix typos

--- slurm_dpclust.sh
+++ slurm_dpclust.sh
@@ -1,7 +1,7 @@
 #$!/user/bin/bash
 
 SAMPLEID=TCGA-4G-AA30
-OUTDIR=/home/groups/Spellmandata/chiotti/gdan_pipelines/heterogeneity/tools/dpclust_out/
+OUTDIR=/home/groups/Spellmandata/chiotti/gdan_pipelines/heterogeneity/tools/dpclust
 
 sbatch --get-user-env dpclust.sh -s $SAMPLEID -o $OUTDIR
 
